c8b947aacc891cd0825a3f8d652019b0c7bcde81
Dan Curtis <dancorpx@gmail.com>
2018-11-08 11:28:13 +0800

    added picture support

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_023917) do
+ActiveRecord::Schema.define(version: 2018_11_05_095613) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
